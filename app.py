b304b3c7319cc49c9dfebc2b3f1bc29ec3e7bd9f
Brando <bbaker.sh@gmail.com>
2024-02-24 18:29:13 -0800

    Add registration functionality

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, render_template, request
 from models import db, connect_db, Users
+from forms import RegisterForm
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -9,12 +10,25 @@ app.config['SECRET_KEY'] = 'shhhhh. its a secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///auth_practice'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.debug = True
 
 connect_db(app)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = Users.register(username, password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
